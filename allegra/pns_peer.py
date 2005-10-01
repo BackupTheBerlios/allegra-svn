817990c864ebd4cb22099c4f2ed29670ae333051
laurentszyster <laurentszyster@5ef75e52-4601-0410-93a0-b417dcdc8c56>
2005-10-01 03:03:04 +0000

    Reflect the change in pns_model.py API

--- allegra/pns_peer.py
+++ allegra/pns_peer.py
@@ -15,7 +15,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 # USA
 
-""
+"http://laurentszyster.be/blog/pns/"
 
 from allegra import async_loop
 from allegra import pns_persistence, pns_semantic, pns_tcp, pns_udp
