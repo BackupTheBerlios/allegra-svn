527ecd6e4ea0cee2316ed709fda24e40a6546c74
laurentszyster <laurentszyster@5ef75e52-4601-0410-93a0-b417dcdc8c56>
2006-01-22 17:10:24 +0000

    Comments out

--- lib/tcp_client.py
+++ lib/tcp_client.py
@@ -15,17 +15,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 # USA
 
-"""DESCRIPTION
-
-One base class to derive TCP client implementation and a few functions 
-that add practical limits to guard a peer against zombie sessions (inactive 
-longer than the limit) and effectively rationate I/O bandwith.
-
-SYNOPSYS
-
-...
-
-"""
+""
 
 import sys, socket, time, collections
 
