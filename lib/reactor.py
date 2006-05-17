e6ba808bc10e2274d49dec3cc5f9c4fb16e19b83
laurentszyster <laurentszyster@5ef75e52-4601-0410-93a0-b417dcdc8c56>
2006-05-17 16:23:50 +0000

    A much simpler Buffer_reactor, to be continued ...

--- lib/reactor.py
+++ lib/reactor.py
@@ -20,53 +20,30 @@ class Buffer_reactor (object):
 
 	"a buffer reactor, usefull for asynchronous/synchronous proxy"
 
-        collector_is_simple = 1
-        buffer_reactor_complete = 0
+        collector_is_simple = True
+        buffer_reactor_complete = False
 
-        def __init__ (self, collector=None):
-                self.buffer_reactor_buffers = []
-                self.collect_incoming_data = \
-                	self.buffer_reactor_buffers.append
-                if collector != None:
-                	self.buffer_reactor (collector)
+        def __init__ (self):
+                self.buffers = []
+                self.collect_incoming_data = self.buffers.append
                 
-        def buffer (self, data):
-        	if data:
-                	self.buffer_reactor_buffers.append (data)
-         	else:
-                	self.buffer_reactor_complete = True
-
 	def found_terminator (self):
 		self.buffer_reactor_complete = True
+                return True
 
         def more (self):
                 try:
-                        return self.buffer_reactor_buffers.pop (0)
+                        return self.buffers.pop (0)
                     
                 except IndexError:
-                	self.buffer_react ()
                         return ''
 
         def producer_stalled (self):
                 return (
-			len (self.buffer_reactor_buffers) == 0 and
+			len (self.buffers) == 0 and
 			self.buffer_reactor_complete == False
                         )
 
-        def buffer_reactor (self, collector):
-        	self.buffer_collector = collector
-        	collector.collect_incoming_data = self.collect_incoming_data
-  		collector.found_terminator = self.buffer_reactor_continue
-
-	def buffer_reactor_continue (self):
-		if self.buffer_collector.found_terminator ():
-			self.buffer_reactor_complete = True
-			self.buffer_collector = None
-			
-	def buffer_react (self):
-		pass # to subclass
-			
-			
 # Note about this implementation
 #
 # The Buffer_reactor is the only practical general-purpose implementation
@@ -78,22 +55,4 @@ class Buffer_reactor (object):
 #	more ()
 #	producer_stalled ()
 #
-# It adds two interfaces:
-#
-#	buffer_reactor (collector)
-#	buffer_reactor_continue ()
-#
-# The first one is usefull to wrap any collector with a stallable buffered
-# producer that can be pushed as a producer.
-#
-# Use of this implementation is simple. To push a collector as a producer
-# onto another channel, simply do:
-#
-#	channel.push (Buffer_reactor (collector (...)))
-#
-# That's all folks.
-#
-# And it is also applicable for any kind of collector, not just channels. You
-# may very well for instance wrap a buffer reactor around the MIME body
-# collector of an HTTP client and set that reactor as the MIME body producer
-# of an HTTP server reactor (see http_proxy.py).
+# It implements both a simple collector and a stallable producer.
