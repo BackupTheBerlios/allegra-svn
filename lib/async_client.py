7e5fa5fb92bd5e79ed9621f6b77c2194da2b975a
laurentszyster <laurentszyster@5ef75e52-4601-0410-93a0-b417dcdc8c56>
2006-06-17 02:48:15 +0000

    A stable and concise API for async_client, allmost there ...

--- lib/async_client.py
+++ lib/async_client.py
@@ -22,39 +22,6 @@ import time, socket, collections
 from allegra import loginfo, async_loop, async_limits
         
 
-class Dispatcher (object):
-        
-        "client mix-in for stream dispatcher"
-        
-        client_when = 0
-
-        def client_connect (self, addr, timeout, family=socket.AF_INET):
-                "create a socket, try to connect it and schedule a timeout"
-                assert not self.connected and self.socket == None
-                try:
-                        self.create_socket (family, socket.SOCK_STREAM)
-                        self.connect (addr)
-                except:
-                        self.loginfo_traceback ()
-                        return False
-                        
-                assert None == self.log ('connect', 'debug')
-                self.client_when = time.time ()
-                async_loop.async_schedule (
-                        self.client_when + timeout, self.client_timeout
-                        )
-                return True
-
-        def client_timeout (self, when):
-                "if not connected and not closing yet, handle close"
-                if not self.connected and not self.closing:
-                        assert None == self.log (
-                                'connect-timeout %f seconds' % (
-                                        when - self.client_when
-                                        ), 'debug'
-                                )
-                        self.handle_close ()
-
 # a pipeline mix-in
 
 class Pipeline (object):
@@ -99,79 +66,68 @@ class Pipeline (object):
                         '%r' % reactor, 'pipeline_push'
                         )
 
+class Dispatcher (object):
+        
+        "client mix-in for stream dispatcher"
+        
+        client_when = 0
+
+        def client_connect (self, addr, timeout, family=socket.AF_INET):
+                "create a socket, try to connect it and schedule a timeout"
+                assert not self.connected and self.socket == None
+                try:
+                        self.create_socket (family, socket.SOCK_STREAM)
+                        self.connect (addr)
+                except:
+                        self.loginfo_traceback ()
+                        return False
+                        
+                assert None == self.log ('connect', 'debug')
+                self.client_when = time.time ()
+                async_loop.async_schedule (
+                        self.client_when + timeout, self.client_timeout
+                        )
+                return True
+
+        def client_timeout (self, when):
+                "if not connected and not closing yet, handle close"
+                if not self.connected and not self.closing:
+                        assert None == self.log (
+                                'connect-timeout %f seconds' % (
+                                        when - self.client_when
+                                        ), 'debug'
+                                )
+                        self.handle_close ()
+
 # A manager abstraction for limited client dispatchers
-
-UNRESOLVED = ((lambda key: True), None)
-
-def nolimit (dispatcher, when):
-        "dummy limit for unlimited dispatcher"
-        return False
-
-def unmanaged (dispatcher):
-        "remove the handle_close decorator from  a client dispatcher"
-        dispatcher.handle_close = dispatcher.client_decorated
-        dispatcher.client_decorated = None
-
-def managed (dispatcher, when):
-        "decorate a client dispatcher's handle_close method"
-        dispatcher.client_decorated = dispatcher.handle_close
-        def handle_close ():
-                unmanaged (dispatcher)
-                dispatcher.handle_close ()
-                dispatcher.client_manager.client_close (dispatcher)
-                
-        dispatcher.handle_close = handle_close
-
-UNLIMITED = (managed, nolimit)
-
 
 class Manager (loginfo.Loginfo):
         
-        client_when = client_dispatched = 0
+        "a connection manager base abstraction for dispatcher"
         
-        def __init__ (
-                self, Dispatcher, 
-                timeout=60, precision=6, 
-                limits=UNLIMITED, resolution=UNRESOLVED,
-                family=socket.AF_INET 
-                ):
+        client_when = client_dispatched = 0
+        ac_in_meter = ac_in_meter = 0
+        
+        def __init__ (self, timeout, precision, family):
                 "initialize a new client manager"
-                self.client_dispatchers = {}
-                self.Client_dispatcher = Dispatcher
-                self.client_family = family
+                self.client_managed = {}
                 self.client_timeout = timeout
                 self.client_precision = precision
-                self.client_decorate, self.client_limit = limits
-                self.client_resolved, self.client_resolve = resolution
-                        
-        def client_dispatcher (self, key):
-                """return a cached or a new dispatcher, maybe resolving and
-                connecting it first, closing it on connection error or if
-                it's socket address cannot be resolved"""
-                try:
-                        return self.client_dispatchers[key]
-                        
-                except KeyError:
-                        pass
-                now = time.time ()
-                dispatcher = self.Client_dispatcher ()
-                dispatcher.client_key = key
-                dispatcher.client_manager = self
-                self.client_decorate (dispatcher, now)
-                self.client_dispatchers[key] = dispatcher
-                if self.client_resolved (key):
+                self.client_family = family
+                unresolved (self)
+                inactive (self, timeout)
+                
+        def client_connect (self, dispatcher, name):
+                dispatcher.client_key = name
+                if self.client_resolved (name):
                         if not dispatcher.client_connect (
-                                key, self.client_timeout,
+                                name, self.client_timeout,
                                 self.client_family
                                 ):
                                 dispatcher.handle_close ()
-                        if len (self.client_dispatchers) == 1:
-                                self.client_start (now)
-                        return dispatcher
+                        return
 
                 assert self.client_resolve != None
-                if len (self.client_dispatchers) == 1:
-                        self.client_start (now)
                 def resolve (addr):
                         if (
                                 addr == None or not
@@ -181,11 +137,8 @@ class Manager (loginfo.Loginfo):
                                         )
                                 ):
                                 dispatcher.handle_close ()
-                self.client_resolve (key, resolve)
-                return dispatcher
+                self.client_resolve (name, resolve)
 
-        __call__ = client_dispatcher
-                
         def client_start (self, when):
                 "handle the client management startup"
                 self.client_when = when
@@ -196,10 +149,10 @@ class Manager (loginfo.Loginfo):
 
         def client_manage (self, when):
                 "test limits overflow, recure or stop"
-                for dispatcher in self.client_dispatchers.values ():
+                for dispatcher in self.client_dispatchers ():
                         if self.client_limit (dispatcher, when):
                                 self.client_overflow (dispatcher)
-                if self.client_dispatchers:
+                if self.client_managed:
                         return (
                                 when + self.client_precision,
                                 self.client_manage
@@ -208,6 +161,9 @@ class Manager (loginfo.Loginfo):
                 self.client_stop (when)
                 return None
         
+        def client_dispatchers (self):
+                return self.client_managed.values ()
+                        
         def client_overflow (self, dispatcher):
                 "assert debug log and close an overflowed dispatcher"
                 assert None == dispatcher.log ('limit overflow', 'debug')
@@ -216,9 +172,9 @@ class Manager (loginfo.Loginfo):
         def client_close (self, dispatcher):
                 "remove the dispatcher from cache and increment dispatched"
                 self.client_dispatched += 1
-                del self.client_dispatchers[dispatcher.client_key]
+                del self.client_managed[dispatcher.client_name]
                 dispatcher.client_manager = None
-                                
+
         def client_stop (self, when):
                 "handle the client management stop"
                 assert None == self.log ('stop %f sec' % (
@@ -227,17 +183,96 @@ class Manager (loginfo.Loginfo):
 
         def client_shutdown (self):
                 "close all client dispatchers when done"
-                for dispatcher in self.client_dispatchers.values ():
+                for dispatcher in self.client_dispatchers ():
                         dispatcher.close_when_done ()
-          
-# unmetered and unlimited              
+                        
 
-def unlimited (client):
-        "unmeter and unlimit I/O"
-        client.client_decorate = managed
-        client.client_limit = nolimit
+class Cache (Manager):
+
+        def __init__ (
+                self, Dispatcher, timeout, precision, 
+                family=socket.AF_INET
+                ):
+                self.client_managed = {}
+                self.Client_dispatcher = Dispatcher
+                self.client_timeout = timeout
+                self.client_precision = precision
+                self.client_family = family
+                unresolved (self)
+                inactive (self, timeout)
+                
+        def __call__ (self, name):
+                """return a cached or a new dispatcher, maybe resolving and
+                connecting it first, closing it on connection error or if
+                it's socket address cannot be resolved"""
+                try:
+                        return self.client_managed[name]
+                        
+                except KeyError:
+                        pass
+                now = time.time ()
+                dispatcher = self.Client_dispatcher ()
+                dispatcher.client_manager = self
+                self.client_decorate (dispatcher, now)
+                self.client_managed[name] = dispatcher
+                self.client_connect (dispatcher, name)
+                if len (self.client_cache) == 1:
+                        self.client_start (now)
+                return dispatcher
+
+
+class Pool (Manager):
         
-# metered I/O
+        def __init__ (
+                self, Dispatcher, name, pool, 
+                timeout=3, precision=1, family=socket.AF_INET
+                ):
+                self.client_managed = []
+                self.client_pool = pool
+                self.client_name = name
+                self.client_called = 0
+                self.Client_dispatcher = Dispatcher
+                self.client_timeout = timeout
+                self.client_precision = precision
+                self.client_family = family
+                unresolved (self)
+                inactive (self, timeout)
+                
+        def __call__ (self):
+                size = self.client_pool
+                if len (self.client_managed) + 1 == size:
+                        self.client_called += 1
+                        return self.client_managed[self.client_called % size]
+                
+                now = time.time ()
+                dispatcher = self.Client_dispatcher ()
+                dispatcher.client_manager = self
+                self.client_decorate (dispatcher, now)
+                self.client_managed.append (dispatcher)
+                self.client_connect (dispatcher, self.client_name)
+                if len (self.client_pool) == 1:
+                        self.client_start (now)
+                return dispatcher
+
+        def client_dispatchers (self):
+                return list (self.client_managed)
+                        
+        def client_close (self, dispatcher):
+                "remove the dispatcher from pool and increment dispatched"
+                self.client_dispatched += 1
+                self.client_managed.remove (dispatcher)
+                dispatcher.client_manager = None
+
+
+# unresolved name
+
+def unresolved (name):
+        "allways resolved for unresolved dispatcher address"
+        client.client_resolved = (lambda name: True)
+        client.client_resolve = None
+
+
+# metered I/O with inactive limit
 
 def unmeter (dispatcher):
         "remove stream decorators from a client dispatcher"
@@ -267,19 +302,11 @@ def meter (dispatcher, when):
                 unmeter (dispatcher)
                 dispatcher.handle_close ()
                 manager = dispatcher.client_manager
-                manager.client_bytes_in += dispatcher.ac_in_meter
-                manager.client_bytes_out += dispatcher.ac_out_meter
+                manager.ac_in_meter += dispatcher.ac_in_meter
+                manager.ac_out_meter += dispatcher.ac_out_meter
                 manager.client_close (dispatcher)
                 
         dispatcher.handle_close = handle_close
-
-def metered (client):
-        client.client_bytes_in = client.client_bytes_out = 0
-        client.client_decorate = meter
-        client.client_limit = nolimit
-
-
-# metered I/O with inactive limit
 
 def inactive (client, timeout):
         "meter I/O and limit inactivity for client streams"
@@ -287,7 +314,6 @@ def inactive (client, timeout):
                 meter (dispatcher, when)
                 dispatcher.limit_inactive = timeout
                 
-        client.client_bytes_in = client.client_bytes_out = 0
         client.client_decorate = decorate
         client.client_limit = async_limits.inactive
 
@@ -331,23 +357,22 @@ def limited (client, timeout, inBps, outBps):
                         undecorate (dispatcher)
                         dispatcher.handle_close ()
                         manager = dispatcher.client_manager
-                        manager.client_bytes_in += dispatcher.ac_in_meter
-                        manager.client_bytes_out += dispatcher.ac_out_meter
+                        manager.ac_in_meter += dispatcher.ac_in_meter
+                        manager.ac_out_meter += dispatcher.ac_out_meter
                         manager.client_close (dispatcher)
                         
                 dispatcher.handle_close = handle_close
         
-        client.client_bytes_in = client.client_bytes_out = 0
         client.client_decorate = decorate
         client.client_limit = async_limits.limit
 
 
 # metered and rationated I/O with inactive limit
 
-def rationated (client, timeout, inBps, outBps):
-        dispatchers = client.client_dispatchers
+def rationed (client, timeout, inBps, outBps):
+        managed = client.client_managed
         limited (
                 client, timeout, 
-                (lambda: int (inBps ()/len (dispatchers))),
-                (lambda: int (outBps ()/len (dispatchers)))
+                (lambda: int (inBps ()/len (managed))),
+                (lambda: int (outBps ()/len (managed)))
                 )
