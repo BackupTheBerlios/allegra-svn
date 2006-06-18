e6396ba1653ea56a74bcaf9113147e848878a04b
laurentszyster <laurentszyster@5ef75e52-4601-0410-93a0-b417dcdc8c56>
2006-06-18 07:26:26 +0000

    A nice test.

--- test/async_client.py
+++ test/async_client.py
@@ -31,7 +31,8 @@ finalization.collect ()
 def push (dispatcher, host):
         if not dispatcher.closing:
                 dispatcher.async_chat_push (
-                        'GET /prompt.xml HTTP/1.1\r\n'
+                        'GET /prompt.xml?PRESTo=async&prompt=xdir(reactor)'
+                        ' HTTP/1.1\r\n'
                         'Host: %s\r\n'
                         'Connection: keep-alive\r\n'
                         '\r\n' % host
@@ -53,9 +54,6 @@ def test_pool (pool):
                 push (pool (), pool.client_name[0])
 
 hosts = ('999.999.999.999', '127.0.0.2', '127.0.0.1')
-manager = async_client.Manager (3, 1)
-cache = async_client.Cache (3, 1)
-pool = async_client.Pool (Chat, (hosts[-1], 80), 2, 3, 1)
 
 def test_managed ():
         test_manager (manager, hosts)
@@ -70,14 +68,25 @@ def test_managed ():
 
 loginfo.log ('test_managed')
 
+manager = async_client.Manager (3, 1)
+cache = async_client.Cache (3, 1)
+pool = async_client.Pool (Chat, (hosts[-1], 80), 2, 3, 1)
 test_managed ()
 
 loginfo.log ('test_limited')
 
+manager = async_client.Manager (6, 1)
+cache = async_client.Cache (6, 1)
+pool = async_client.Pool (Chat, (hosts[-1], 80), 2, 6, 1)
 for client in (manager, cache, pool):
-        async_client.limited (client, 3, (lambda: 1024), (lambda: 1024))
+        async_client.limited (client, 6, (lambda: 1024), (lambda: 1024))
 test_managed ()
 
+loginfo.log ('test_rationed')
+
+manager = async_client.Manager (6, 1)
+cache = async_client.Cache (6, 1)
+pool = async_client.Pool (Chat, (hosts[-1], 80), 2, 6, 1)
 for client in (manager, cache, pool):
-        async_client.rationed (client, 3, 1024, 1024)
+        async_client.rationed (client, 6, 4096, 4096)
 test_managed ()
