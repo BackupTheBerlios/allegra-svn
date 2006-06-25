ffa0235630c08356977954062af5fdadd4c926b6
laurentszyster <laurentszyster@5ef75e52-4601-0410-93a0-b417dcdc8c56>
2006-06-25 13:13:47 +0000

    a simplistic test for async_server, needs a face-off test for async_client ...

---
+++ test/async_server.py
@@ -0,0 +1,39 @@
+# TCP/IP decorators
+
+import socket
+
+from allegra import async_net, async_server
+
+# 
+
+listen = async_server.Listen (
+    async_net.Dispatcher, ('127.0.0.1', 1234),
+    256, 6, 5, socket.AF_INET 
+    )
+async_server.catch_shutdown (listen)
+del listen
+
+#
+
+from allegra import async_loop
+
+async_loop.dispatch ()
+
+# public
+
+from allegra import async_chat
+
+listen = async_server.Listen (
+    async_chat.Dispatcher, ('0.0.0.0', 1234),
+    2, 6, 5, socket.AF_INET 
+    )
+listen.server_resolved = (lambda addr: addr[0])
+async_server.rationed (listen, 3, 1<<14, 1<<16)
+async_server.catch_shutdown (listen)
+del listen
+
+async_loop.dispatch ()
+
+from allegra import finalization
+
+finalization.collect ()
