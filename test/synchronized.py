53dc731558415b79086d8f936246b1cadf39c4ba
laurentszyster <laurentszyster@5ef75e52-4601-0410-93a0-b417dcdc8c56>
2006-06-30 10:35:52 +0000

    test and bugfix for synchronized ... again

---
+++ test/synchronized.py
@@ -0,0 +1,34 @@
+import time
+
+from allegra import loginfo, async_loop, synchronized
+
+def test_producer (when):
+        loginfo.log ('test File_producer')
+        p = synchronized.File_producer ('test.txt')
+        def test_produce (when):
+                if p.producer_stalled ():
+                        async_loop.schedule (
+                                when + async_loop.async_timeout, test_produce
+                                )
+                else:
+                        loginfo.log (p.more (), 'p.more ()')
+                
+        async_loop.schedule (
+                when + async_loop.async_timeout, test_produce
+                )
+
+def test_collector (when):
+        loginfo.log ('test File_collector')
+        c = synchronized.File_collector ('test.txt')
+        c.collect_incoming_data ('this is a test\n')
+        c.collect_incoming_data ('this is a test\n')
+        c.collect_incoming_data ('this is a test\n')
+        c.found_terminator ()
+        async_loop.schedule (
+                when + async_loop.async_timeout, test_producer
+                )
+        
+async_loop.schedule (
+        time.time () + async_loop.async_timeout, test_collector
+        )
+async_loop.dispatch ()
