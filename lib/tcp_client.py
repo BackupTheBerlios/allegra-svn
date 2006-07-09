a139297bf41e8e4e1b29010be52a142ddaf277f1
laurentszyster <laurentszyster@5ef75e52-4601-0410-93a0-b417dcdc8c56>
2006-07-09 00:43:57 +0000

    A full API and first conveniences for TCP/IP, lacks test and a revamped DNS client ...

--- lib/tcp_client.py
+++ lib/tcp_client.py
@@ -17,231 +17,82 @@
 
 ""
 
-import sys, socket, time, collections
+import socket
 
-from allegra import (
-	loginfo, async_loop, async_limits, async_net, async_chat
-        )
+from allegra import async_client, dns_client
+
+
+def connect (
+        dispatcher, name, timeout, 
+        resolved=dns_client.ip_resolved, resolve=resolve_dns_A
+        ):
+        "resolve and maybe connect a dispatcher, close on error"
+        if resolved (name):
+                if not async_client.connect (
+                        dispatcher, name, timeout, socket.AF_INET
+                        ):
+                        dispatcher.handle_close ()
+                        return False
+                
+                return True
+
+        if resolve == None:
+                dispatcher.handle_close ()
+                return False
         
-def FourKBps (): return 4096
+        def resolve (addr):
+                if addr == None or not async_client.connect (
+                        dispatcher, addr, timeout, socket.AF_INET
+                        ):
+                        dispatcher.handle_close ()
+        client_resolve (name, resolve)
+        return True
 
 
-class TCP_client_channel (object):
-
-	tcp_client_defer = None
-	
-        def tcp_connect (self, addr, timeout=3):
-        	"create a socket and try to connect to addr, return success"
-		if self.connected:
-			return True
-
-                self.create_socket (socket.AF_INET, socket.SOCK_STREAM)
-                try:
-			self.connect (addr)
-		except:
-			self.loginfo_traceback ()
-			return False
-			
-		assert None == self.log ('connect %s %d' % addr, 'debug')
-		async_loop.schedule (
-			time.time () + timeout, self.tcp_timeout
-			)
-		return True
-
-	def tcp_timeout (self, when):
-		"if connected, continue to defer, otherwise handle close"
-		if not self.connected and not self.closing:
-			assert None == self.log (
-				'connect-timeout', 'debug'
-				)
-			self.handle_close ()
-
-
-class TCP_client_net (async_net.Dispatcher, TCP_client_channel):
-	
-	"The simplest netstring client, a netline logger."
-
-
-class TCP_client_line (async_chat.Dispatcher, TCP_client_channel):
-	
-	"The simplest asynchat client, a netline logger."
-
-        echo_line = ''
-                
-	def __repr__ (self):
-		return 'line-client-channel id="%x"' % id (self)
-
-        def collect_incoming_data (self, data):
-                self.echo_line += data
-                
-        def found_terminator (self):
-                self.log (self.echo_line)
-                self.echo_line = ''
- 
-
-# A manager interface for TCP/IP clients
+def dns_A_resolved (connections, resolver=None):
+        if resolver == None:
+                resolver = DNS_client (dns_servers ())
+        def async_client_resolve (name, resolve):
+                def dns_resolve (resolved):
+                        if resolved.dns_resources:
+                                resolve (resolved.dns_resources[0])
+                        else:
+                                resolve (None)
+                resolver.dns_resolve ((name, 'A'), dns_resolve)
         
-class TCP_client (loginfo.Loginfo):
-	
-	TCP_CLIENT_CHANNEL = TCP_client_line
-	
-	def __init__ (self, timeout=60, precision=10):
-		self.tcp_client_timeout = timeout
-		self.tcp_client_precision = precision
-		self.tcp_client_channels = {}
-		
-	def tcp_client (self, addr, timeout=3):
-		try:
-			return self.tcp_client_channels[addr]
-			
-		except KeyError:
-			channel = self.tcp_client_channel (addr)
-                        if channel.tcp_connect (addr, timeout):
-                                return channel
-                                
-                        return
-	
-	def tcp_client_channel (self, addr):
-                now = time.time ()
-		channel = self.TCP_CLIENT_CHANNEL ()
-                channel.tcp_client_key = addr
-		async_limits.meter_recv (channel, now)
-		async_limits.meter_send (channel, now)
-                def handle_close ():
-                        self.tcp_client_close (channel)
-		channel.handle_close = handle_close
-		self.tcp_client_channels[addr] = channel
-		if len (self.tcp_client_channels) == 1:
-	                assert None == self.log ('defer-start', 'debug')
-			async_loop.schedule (
-				time.time () + self.tcp_client_precision, 
-				self.tcp_client_defer
-				)
-		return channel
-		
-	def tcp_client_defer (self, when):
-                if self.tcp_client_channels:
-	                for channel in self.tcp_client_channels.values ():
-	                        self.tcp_client_inactive (channel, when)
-                        return (
-                                when + self.tcp_client_precision,
-                                self.tcp_client_defer
-                                ) # continue to defer
-                
-        	self.tcp_client_stop ()
-                return None
+        connections.client_resolved = ip_resolved
+        connections.client_resolve = async_client_resolve
 
-	def tcp_client_inactive (self, channel, when):
-		if not channel.closing and channel.connected and (
-			when - max (
-				channel.ac_in_when, channel.ac_out_when
-				)
-			) > self.tcp_client_timeout:
-			assert None == channel.log ('inactive', 'debug')
-			channel.handle_close ()
+# conveniences for named TCP/IP connections
 
-        def tcp_client_close (self, channel):
-                assert None == channel.log (
-                        'in="%d" out="%d"' % (
-                                channel.ac_in_meter, 
-                                channel.ac_out_meter
-                                ),  'debug'
-                        )
-                del channel.recv, channel.send, channel.handle_close
-                channel.handle_close ()
-                del self.tcp_client_channels[channel.tcp_client_key]
+def Connections (
+        timeout, precision, 
+        resolution=dns_A_resolved
+        ):
+        connections = async_clientConnections (
+                timeout, precision, socket.AF_INET
+                )
+        connections.client_resolved = resolution (connections)
+        return connections
 
-        def tcp_client_shutdown (self):
-                for channel in self.tcp_client_channels.values ():
-                        channel.close_when_done ()        
-                        		
-	def tcp_client_stop (self):
-		assert None == self.log ('stop', 'debug')
+def Cache (
+        Dispatcher, timeout, precision, 
+        resolution=dns_A_resolved
+        ):
+        connections = async_client.Connections (
+                Dispatcher, timeout, precision, socket.AF_INET
+                )
+        connections.client_resolved = resolution (connections)
+        return connections
 
+def Pool (
+        Dispatcher, name, size, timeout, precision, 
+        resolution=dns_A_resolved
+        ):
+        connections = async_client.Connections (
+                Dispatcher, timeout, precision, socket.AF_INET
+                )
+        connections.client_resolved = resolution (connections)
+        return connections
 
-def tcp_client_throttle_defer (channel, when):
-	async_limits.throttle_in (channel, when)
-	async_limits.throttle_out (channel, when)
-	return tcp_client_inactive (channel, when)
-
-def tcp_client_throttle (
-	channel, 
-	throttle_in=FourKBps, 
-	throttle_out=FourKBps, 
-	inactive=60, precision=10
-	):
-	TCP_client_limit (channel, inactive, precision)
-	channel.tcp_client_defer = tcp_client_throttle
-	return channel
-
-
-def tcp_client_throttle_out_defer (channel, when):
-	async_limits.throttle_out (channel, when)
-	return tcp_client_inactive (channel, when)
-
-def tcp_client_throttle_out (
-	channel, Bps=FourKBps, inactive=60, precision=10
-	):
-	tcp_client_limit (channel, inactive, precision)
-	channel.async_throttle_out_Bps = Bps
-	channel.tcp_client_defer = tcp_client_throttle_out_defer
-	return channel
-
-
-def tcp_client_throttle_in_defer (channel, when):
-	async_limits.throttle_in (channel, when)
-	return tcp_client_inactive (channel, when)
-
-def tcp_client_throttle_in (
-	channel, Bps=FourKBps, inactive=60, precision=10
-	):
-	tcp_client_limit (channel, inactive, precision)
-	channel.async_throttle_in_Bps = Bps
-	channel.tcp_client_defer = tcp_client_throttle_in_defer
-	return channel
-	
-
-class Pipeline (object):
-
-	pipeline_sleeping = False
-	pipeline_keep_alive = False
-
-	def __init__ (self, requests=None, responses=None):
-		self.pipeline_requests = requests or collections.deque ()
-		self.pipeline_responses = responses or collections.deque ()
-
-	def pipeline (self, request):
-		self.pipeline_requests.append (request)
-		if self.pipeline_sleeping:
-			self.pipeline_sleeping = False
-			self.pipeline_wake_up ()
-
-	def pipeline_wake_up (self):
-		# pipelining protocols, like HTTP/1.1 or ESMPT
-                requests = self.pipeline_requests
-		if self.pipeline_requests:
-			while self.pipeline_requests:
-				reactor = self.pipeline_requests.popleft ()
-                                self.pipeline_push (reactor)
-				self.pipeline_responses.append (reactor)
-		self.pipeline_sleeping = True
-
-	def pipeline_wake_up_once (self):
-		# synchronous protocols, like HTTP/1.0 or SMTP
-		if self.pipeline_requests:
-			reactor = self.pipeline_requests.popleft ()
-                        self.pipeline_push (reactor)
-			self.pipeline_responses.append (reactor)
-			self.pipeline_sleeping = False
-		else:
-			self.pipeline_sleeping = True
-                        
-        def pipeline_push (self, reactor):
-                assert None == loginfo.log (
-                        '%r' % reactor, 'pipeline_push'
-                        )
-                        
-#
-# TODO: factor out a client_* API out of tcp_client_* interfaces, 
-#       then move it to its own client.py module.
-#                        
+# 
