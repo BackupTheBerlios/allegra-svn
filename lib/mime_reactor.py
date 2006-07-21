46ed0862d51dd21a47bb53352708f76345c49168
laurentszyster <laurentszyster@5ef75e52-4601-0410-93a0-b417dcdc8c56>
2006-07-21 18:00:25 +0000

    A work in progress ...

--- lib/mime_reactor.py
+++ lib/mime_reactor.py
@@ -29,7 +29,7 @@ class MIME_producer (object):
 	def more (self):
 		data = ''.join (self.mime_producer_lines)
                 if self.mime_producer_body == None:
-                        self.mime_producer_lines = ()
+                        self.mime_producer_lines == ()
                         return data
                 
                 self.more = self.mime_producer_body.more
@@ -41,96 +41,159 @@ class MIME_producer (object):
                 return self.mime_producer_lines == None
 
 
-class Escaping_producer (object):
-
-	"A producer that escapes a sequence of characters"
-
-	# Common usage: escaping the CRLF.CRLF sequence in SMTP, NNTP, etc ...
-
-	def __init__ (self, producer, esc_from='\r\n.', esc_to='\r\n..'):
-		self.producer = producer
-		self.esc_from = esc_from
-		self.esc_to = esc_to
-		self.buffer = ''
-		self.producer_stalled = self.producer.producer_stalled
-
-	def more (self):
-		buffer = self.buffer + self.producer.more ()
-		if buffer:
-			buffer = string.replace (
-				buffer, self.esc_from, self.esc_to
-				)
-			i = async_chat.find_prefix_at_end (
-				buffer, self.esc_from
-				)
-			if i:
-				# we found a prefix
-				self.buffer = buffer[-i:]
-				return buffer[:-i]
-			
-			# no prefix, return it all
-			self.buffer = ''
-			return buffer
-			
-		return buffer
-
-
 class MIME_collector (object):
-	
-	"""A collector implementation for all MIME collectors protocols,
-	like MULTIPART but also POP, HTTP, etc ..."""
-	
-	collector_is_simple = False
-	
-	mime_collector_buffer = ''
-	mime_collector_lines = \
-		mime_collector_headers = mime_collector_body = None
+        
+        """A collector implementation for all MIME collectors protocols,
+        like MULTIPART but also POP, HTTP, etc ..."""
+        
+        collector_is_simple = False
+        
+        mime_collector_buffer = ''
+        mime_collector_lines = \
+                mime_collector_headers = mime_collector_body = None
+                
+        def collect_incoming_data (self, data):
+                # collect the MIME body or its headers
+                if self.mime_collector_body == None:
+                        self.mime_collector_buffer += data
+                else:
+                        self.mime_collector_body.collect_incoming_data (data)
 
-	def collect_incoming_data (self, data):
-		# collect the MIME body or its headers
-		if self.mime_collector_body == None:
-			self.mime_collector_buffer += data
-		else:
-			self.mime_collector_body.collect_incoming_data (data)
-
-	def found_terminator (self):
-		if self.mime_collector_body == None:
-			# MIME headers collected, clear the buffer, split the
-			# headers and continue ...
-			self.mime_collector_lines = mime_headers.split (
-				self.mime_collector_buffer
-				)
-			self.mime_collector_buffer = ''
-			return self.mime_collector_continue ()
+        def found_terminator (self):
+                if self.mime_collector_body == None:
+                        # MIME headers collected, clear the buffer, split the
+                        # headers and continue ...
+                        self.mime_collector_lines = mime_headers.split (
+                                self.mime_collector_buffer
+                                )
+                        return self.mime_collector_continue ()
                         
-		elif self.mime_collector_body.found_terminator ():
-			# if the MIME body final terminator is reached,
-			# finalize it and reset the state of the collector
-			self.mime_collector_buffer = '' # ?
-			return self.mime_collector_finalize ()
+                elif self.mime_collector_body.found_terminator ():
+                        # if the MIME body final terminator is reached,
+                        # finalize it and reset the state of the collector
+                        # self.mime_collector_buffer = '' # ?
+                        return self.mime_collector_finalize ()
 
-	def mime_collector_continue (self):
-		self.mime_collector_body = reactor.Buffer ()
+        def mime_collector_continue (self):
+                self.set_terminator (None)
+                self.mime_collector_body = reactor.Buffer ()
                 return False
 
-	def mime_collector_finalize (self, reactor):
+        def mime_collector_finalize (self, reactor):
                 # reset MIME collector's state
+                self.mime_collector_buffer = ''
                 self.mime_collector_headers = \
                         self.mime_collector_lines = \
                         self.mime_collector_body = None
-	        return False
+                return True # stalled!
 
+
+class MIME_reactor (
+        MIME_collector, MIME_producer, finalization.Finalization
+        ):
+
+        def __init__ (self, headers=None, set_terminator=None):
+                # first maybe attribute another set_terminator method,
+                if set_terminator != None:
+                        self.set_terminator = set_terminator
+                if headers == None:
+                        # if no headers have been provided, get them and so
+                        # set the terminator to '\r\n\r\n'
+                        #
+                        self.set_terminator ('\r\n\r\n')
+                else:
+                        # or consider the headers as allready collected and
+                        # immediately set the body collector to the result of
+                        # the continuation ...
+                        #
+                        self.mime_collector_continue (headers)
+
+        def mime_collector_continue (self, headers):
+                self.mime_producer_headers = headers
+                self.mime_collector_body = self.mime_producer_body = \
+                        buffer.Reactor ()
+                return False
+
+
+class Escaping_producer (object):
+
+        "A producer that escapes a stream of characters"
+
+        # Common usage: escaping the CRLF.CRLF sequence in SMTP, NNTP, etc ...
+
+        def __init__ (self, producer, esc_from='\r\n.', esc_to='\r\n..'):
+                self.producer = producer
+                self.esc_from = esc_from
+                self.esc_to = esc_to
+                self.buffer = ''
+                self.producer_stalled = self.producer.producer_stalled
+
+        def more (self):
+                buffer = self.buffer + self.producer.more ()
+                if buffer:
+                        buffer = string.replace (
+                                buffer, self.esc_from, self.esc_to
+                                )
+                        i = async_chat.find_prefix_at_end (
+                                buffer, self.esc_from
+                                )
+                        if i:
+                                # we found a prefix
+                                self.buffer = buffer[-i:]
+                                return buffer[:-i]
+                        
+                        # no prefix, return it all
+                        self.buffer = ''
+                        return buffer
+                        
+                return buffer
+
+
+class Escaping_collector (object):
+
+        "A collector that escapes a stream of characters"
+
+        def __init__ (self, collect, esc_from='\r\n.', esc_to='\r\n..'):
+                self.escaped = collect
+                self.esc_from = esc_from
+                self.esc_to = esc_to
+                self.buffer = ''
+
+        def collect_incoming_data (self, data):
+                buffer = string.replace (
+                        self.buffer + data, self.esc_from, self.esc_to
+                        )
+                i = async_chat.find_prefix_at_end (
+                        buffer, self.esc_from
+                        )
+                if i > 0:
+                        self.escaped.collect_incoming_data (buffer[-i:])
+                        self.buffer = buffer[:-i]
+                else:
+                        self.escaped.collect_incoming_data (buffer)
+                        self.buffer = ''
+
+        def found_terminator (self):
+                self.escaped.found_terminator
+
+
+class MULTIPART_producer (producer.Composite):
+
+        def __init__ (self, parts, headers=None):
+                self.mime_producer_headers = headers or {}
+                boundary = '\r\n\r\n--%x' % id (self)
+                
 
 class MULTIPART_collector (object):
 	
 	collector_is_simple = False
+        multipart_collect = None
 
-        def __init__ (self, mime_collector, multipart_factory):
-        	self.mime_collector = mime_collector
-                self.multipart_factory = multipart_factory
+        def __init__ (self, mime_collector, Collect):
+                self.multipart_Collect = Collect
                 self.multipart_buffer = ''
-                self.multipart_part = None
-                self.multipart_parts = {}
+                self.collect_incoming_data = \
+                        self.multipart_buffer.__add__
 		self.multipart_boundary = '\r\n\r\n--' + \
 			mime_headers.get_parameter (
 				mime_collector.mime_collector_headers[
@@ -140,13 +203,8 @@ class MULTIPART_collector (object):
 		self.set_terminator = mime_collector.set_terminator
 		self.set_terminator (self.multipart_boundary[4:])
 		
-        def multipart_collect (self, data):
-		self.multipart_buffer += data
-                
-        collect_incoming_data = multipart_collect
-
 	def multipart_found_next (self):
-		if self.multipart_buffer == '--':
+		if self.multipart_buffer in ('--', ''):
 			self.set_terminator = self.mime_collector = \
                                 self.collect_incoming_data = \
                                 self.found_terminator = None
@@ -155,23 +213,27 @@ class MULTIPART_collector (object):
 		else:
 			self.set_terminator ('\r\n\r\n')
 			self.found_terminator = self.multipart_found_headers
-			self.collect_incoming_data = self.multipart_collect
+                        self.multipart_buffer = ''
+                        self.collect_incoming_data = \
+                                self.multipart_buffer.__add__
 			return False
 
 	def multipart_found_headers (self):
 		headers = mime_headers.map (
 			mime_headers.split (self.multipart_buffer)
 			)
-                collect = self.multipart_factory (headers)
+                collect = self.multipart_Collect (headers)
 		if not collect.collector_is_simple:
 			collect = collector.Simple (collect)
-		self.multipart_parts.append (collect)
+		# self.multipart_parts.append (collect)
+                self.multipart_collect = collect
 		self.collect_incoming_data = collect.collect_incoming_data		
 		self.found_terminator = self.multipart_found_boundary
 		self.set_terminator (self.multipart_boundary)
 		return False
 
 	def multipart_found_boundary (self):
+                self.multipart_collect.found_terminator ()
 		self.set_terminator (2)
 		self.found_terminator = self.multipart_found_next
 		return False
@@ -199,36 +261,6 @@ class MULTIPART_collector (object):
                                 self.mime_collector.mime_collector_continue
                                 )
 
-
-class MIME_reactor (
-	MIME_collector, MIME_producer, finalization.Finalization
-	):
-
-	def __init__ (self, headers=None, set_terminator=None):
-		# first maybe attribute another set_terminator method,
-		if set_terminator != None:
-			self.set_terminator = set_terminator
-		if headers == None:
-			# if no headers have been provided, get them and so
-			# set the terminator to '\r\n\r\n'
-			#
-			self.set_terminator ('\r\n\r\n')
-		else:
-			# or consider the headers as allready collected and
-			# immediately set the body collector to the result of
-			# the continuation ...
-			#
-			self.mime_collector_headers = headers
-			self.mime_collector_body = \
-				self.mime_collector_continue ()
-
-	def mime_collector_continue (self):
-                # a proxy buffer implementation
-		body = reactor.Buffer ()
-                self.mime_producer_headers = self.mime_collector_headers
-                self.mime_producer_body = body
-		return body
-			
 
 if __name__ == '__main__':
 	from allegra import loginfo
