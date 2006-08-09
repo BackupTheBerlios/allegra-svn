992405c1149d0f12ea86a29956222b80a63efe0a
laurentszyster <laurentszyster@5ef75e52-4601-0410-93a0-b417dcdc8c56>
2006-08-09 22:43:18 +0000

    Sync test order with the blog's doc

--- test/ip_peer.py
+++ test/ip_peer.py
@@ -1,8 +1,8 @@
 from allegra import loginfo, ip_peer
         
 s = ip_peer.host_ip
-loginfo.log ('%r' % ip_peer.is_ip (s))
 loginfo.log ('%r' % s)
+loginfo.log ('%r' % ip_peer.is_ip (s))
 l = ip_peer.ip2long (s)
 loginfo.log ('%r' % l)
 loginfo.log ('%r' % (ip_peer.long2ip (l)))
