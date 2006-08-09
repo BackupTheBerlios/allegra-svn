2b4a6eeb1bd79a3bbe3ab1c4002e323b8f2b7718
laurentszyster <laurentszyster@5ef75e52-4601-0410-93a0-b417dcdc8c56>
2006-08-09 02:25:37 +0000

    Test for lib/ip_peer.py functions

---
+++ test/ip_peer.py
@@ -0,0 +1,8 @@
+from allegra import loginfo, ip_peer
+        
+s = ip_peer.host_ip
+loginfo.log ('%r' % s)
+l = ip_peer.ip2long (s)
+loginfo.log ('%r' % l)
+loginfo.log ('%r' % (ip_peer.long2ip (l)))
+loginfo.log ('%r' % ip_peer.in_addr_arpa (s))
