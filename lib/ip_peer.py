db4013e1e339c7ad986f4531a5a79ed6cfc7cc77
laurentszyster <laurentszyster@5ef75e52-4601-0410-93a0-b417dcdc8c56>
2006-08-11 17:11:39 +0000

    Commented out the is_local and is_private test functions, a simpler test in the address string is faster ... and more practical in any case.

--- lib/ip_peer.py
+++ lib/ip_peer.py
@@ -20,6 +20,13 @@
 import socket, random
 
 
+def _host_ip ():
+        "resolve the host IP address synchronously"
+        return socket.gethostbyname (socket.gethostname ())
+
+host_ip = _host_ip ()
+
+
 def is_ip (name):
         "returns True if the name is a valid IPv4 address string"
         return len (tuple ((
@@ -53,29 +60,21 @@ def long2ip (i):
         i, rest = divmod (rest, 256)
         return '%s.%d.%d' % (s, i, rest)
 
-
-def _host_ip ():
-        "resolve the host IP address synchronously"
-        return socket.gethostbyname (socket.gethostname ())
-
-host_ip = _host_ip ()
-
-
-local_network = ip2long ('127.0.0.0')
-private_network_A = ip2long ('10.0.0.0')
-private_network_C = ip2long ('192.168.0.0')
-
-def is_local (ipl):
-        "returns True if the IP long belongs to the IPv4 local network"
-        return ipl | local_network == ipl
-
-def is_private (ipl):
-        "returns True if the IP long belongs to a IPv4 private network"
-        return (
-                (ipl | private_network_A == ipl) or 
-                (ipl | private_network_C == ipl)
-                )
-
+#
+#local_network = ip2long ('127.0.0.0')
+#private_network_A = ip2long ('10.0.0.0')
+#private_network_C = ip2long ('192.168.0.0')
+#
+#def is_local (ipl):
+#        "returns True if the IP long belongs to the IPv4 local network"
+#        return ipl | local_network == ipl
+#
+#def is_private (ipl):
+#        "returns True if the IP long belongs to a IPv4 private network"
+#        return (
+#                (ipl | private_network_A == ipl) or 
+#                (ipl | private_network_C == ipl)
+#                )
 
 def udp_bind (dispatcher, ip=None, port=None):
         """bind an async_core.Dispatcher () to an address and return True
