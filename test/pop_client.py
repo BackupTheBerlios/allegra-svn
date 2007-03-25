3d5fc45d024dfd96d255983424089d5b487f9921
laurentszyster <laurentszyster@5ef75e52-4601-0410-93a0-b417dcdc8c56>
2007-03-25 02:06:15 +0000

    New, old, broken and working tests ...

--- test/pop_client.py
+++ test/pop_client.py
@@ -1,3 +1,19 @@
+# Copyright (C) 2007 Laurent A.V. Szyster
+#
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of version 2 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+#    http://www.gnu.org/copyleft/gpl.html
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# You should have received a copy of the GNU General Public License
+# along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
 import sys
 
 from allegra import loginfo, async_loop, collector, pop_client
@@ -7,18 +23,18 @@ try:
     username = sys.argv[1]
     password = sys.argv[2]
     if len (sys.argv) > 3:
-        uid = sys.argv[3]
+        host = sys.argv[3]
         if len (sys.argv) > 4:
-            host = sys.argv[4]
+            port = int (sys.argv[4])
             if len (sys.argv) > 5:
-                port = int (sys.argv[5])
+                order = sys.argv[5]
             else:
-                port = 110
+                order = None
         else:
-            host = '127.0.0.1'
             port = 110
     else:
-        pass
+        host = '127.0.0.1'
+        port = 110
 except:
     loginfo.log (
         'Invalid arguments,'
@@ -35,23 +51,30 @@ if mailbox == None:
         'fatal'
         )
     sys.exit (2)
+
+if __debug__:
+    Collect = (lambda: collector.LOGINFO)
 else:
-    if __debug__:
-        COLLECT = collector.LOGINFO
+    Collect = (lambda: collector.DEVNULL)
+        
+def capable ():
+    if order == None and not (
+        mailbox.pop_capabilities != None and
+        'UIDL' in mailbox.pop_capabilities
+        ):
+        mailbox.pop_quit ()
     else:
-        COLLECT = collector.DEVNULL
-    def capable ():
-        if not (
-            mailbox.pop_capabilities != None and
-            'UIDL' in mailbox.pop_capabilities
-            ):
-            mailbox.pop_quit ()
         def authorized ():
-            mailbox.pop_uidl_and_retr (uid, (lambda: COLLECT))
-            mailbox.pop_quit_after_last ()
+            if order == None:
+                mailbox.pop_uidl_and_retr (set (), Collect)
+            else:
+                mailbox.pop_retr_one (order, Collect)
+                mailbox.pipeline_after_last (mailbox.pop_quit)
+                    
         mailbox.pop_authorize (
             username, password, authorized, mailbox.pop_quit
             )
-    mailbox.pop_capable (capable)
+                
+mailbox.pop_capable (capable)
 async_loop.dispatch ()
 sys.exit (0)
