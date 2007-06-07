de8c216db75e3e4e656969a841083e2c9426647d
laurentszyster <laurentszyster@5ef75e52-4601-0410-93a0-b417dcdc8c56>
2007-06-07 21:54:09 +0000

    copyright & licence update

--- lib/finalization.py
+++ lib/finalization.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2005 Laurent A.V. Szyster
+# Copyright (C) 2005-2007 Laurent A.V. Szyster
 #
 # This library is free software; you can redistribute it and/or modify
 # it under the terms of version 2 of the GNU General Public License as
@@ -12,8 +12,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this library; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-# USA
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 "http://laurentszyster.be/blog/finalization/"
 
